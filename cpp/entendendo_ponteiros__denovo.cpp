f9c10402dabc3cc68f04f6adaa16cdf2b902bfe5
Caio-spr <csr.caiospinaramos@gmail.com>
2021-08-21 01:54:00 -0300

    .

--- cpp/entendendo_ponteiros__denovo.cpp
+++ cpp/entendendo_ponteiros__denovo.cpp
@@ -7,8 +7,16 @@ int main(){
     
     int a = 1;
     cout << &a << endl;
-    int* b = &a;
-    cout << *b;
-    
+    int *b = &a;
+    cout << *b << endl;
+    int c = *b;
+    cout << c << endl;
+
+    /*
+    c = 5;
+    cout << a << endl;
+    cout << *b << endl;
+    cout << c << endl;  
     return 0;
+    */
 }
